2c8ffdf9a9f4b3794c9de3660059b05ad553cb08
urly3 <>
2024-05-25 04:14:43 +0100

    doing the data. todo: - come up with a representation of cost / impact. - highest scores, lowest scores / acc.
    ok.

--- Program.cs
+++ Program.cs
@@ -1,58 +1,55 @@
-﻿using System.Net;
-using System.Runtime.ExceptionServices;
-using Microsoft.VisualBasic;
-
 namespace OsuMultiAnalyser;
 
-class Program
-{
-    static void Main(string[] args)
-    {
+class Program {
+    static void Main(string[] args) {
         var lobby = Lobby.Parse(111534249);
         _ = lobby.users ?? throw new Exception("no users");
         _ = lobby.events ?? throw new Exception("no events");
 
 
-        foreach (var gameEvent in lobby.events.Where(e => e.game != null))
-        {
+        foreach (var gameEvent in lobby.CompletedGames) {
             var game = gameEvent.game;
-            if (game.beatmap == null || game.beatmap.beatmapset == null)
-            {
+            if (game?.beatmap == null || game.beatmap.beatmapset == null) {
                 Console.WriteLine("deleted mapset");
-            }
-            else
-            {
-                Console.Write(game.beatmap.beatmapset.artist);
+            } else {
+                Console.Write((string?)game.beatmap.beatmapset.artist);
                 Console.Write(" - ");
-                Console.Write(game.beatmap.beatmapset.title);
-                Console.Write("\n");
+                Console.Write((string?)game.beatmap.beatmapset.title);
+                Console.Write('\n');
             }
-
-            foreach (var user in lobby.users)
-            {
-                var avgScore = user.CaculateAverageScore(game.scores);
-                if (avgScore == null)
-                {
-                    continue;
-                }
-
-                Console.WriteLine(user.username);
-                Console.Write("avg score: ");
-                Console.WriteLine(avgScore);
-            }
-
-            Console.WriteLine();
-
         }
 
-        lobby.GetWinningTeam();
+        Console.Write("\n");
+
+        lobby.CalculateStats();
 
         Console.Write("winning team: ");
         Console.WriteLine(lobby.WinningTeam);
 
         Console.Write("score: ");
-        Console.Write(lobby.BlueWins.ToString());
-        Console.Write(" - ");
-        Console.WriteLine(lobby.RedWins.ToString());
+        Console.Write(lobby.BlueWins.ToString() + " - " + lobby.RedWins.ToString() + "\n\n");
+
+        Console.WriteLine("average player stats: ");
+
+        foreach (var user in lobby.users)
+        {
+            Console.WriteLine(user.username);
+            Console.WriteLine("avg score:\t" + user.AverageScore.ToString("n0"));
+            Console.WriteLine("avg acc:\t" + user.AverageAccuracy.ToString("f2") + '%');
+            Console.WriteLine("maps played:\t" + user.MapsPlayed.ToString());
+            Console.Write('\n');
+        }
+
+        Console.Write('\n');
+
+        Console.WriteLine("highest avg score: ");
+        Console.WriteLine(lobby.users.First(u => u.id == lobby.HighestAverageScore.Item1).username);
+        Console.WriteLine(lobby.HighestAverageScore.Item2);
+
+        Console.Write('\n');
+
+        Console.WriteLine("highest avg accuracy: ");
+        Console.WriteLine(lobby.users.First(u => u.id == lobby.HighestAverageAccuracy.Item1).username);
+        Console.WriteLine(lobby.HighestAverageAccuracy.Item2);
     }
 }
--- Things.cs
+++ Things.cs
@@ -1,11 +1,9 @@
 using System.Net;
-using System.Security.Cryptography;
 using System.Text.Json.Serialization;
 
 namespace OsuMultiAnalyser;
 
-public class Lobby
-{
+public class Lobby {
     public Match? match { get; set; }
     public List<Event>? events { get; set; }
     public List<User>? users { get; set; }
@@ -13,23 +11,25 @@ public class Lobby
     public long latest_event_id { get; set; }
     public object? current_game_id { get; set; }
 
+    public List<Event> AbandonedGames { get; set; } = new();
+    public List<Event> CompletedGames { get; set; } = new();
     public string WinningTeam { get; set; } = "";
-    public int RedWins { get; set; }
-    public int BlueWins { get; set; }
+    public int RedWins { get; set; } = 0;
+    public int BlueWins { get; set; } = 0;
 
-    public static Lobby Parse(int id)
-    {
+    public (long, int) HighestAverageScore { get; set; } = (0, 0);
+    public (long, float) HighestAverageAccuracy { get; set; } = (0, 0);
+
+    public static Lobby Parse(int id) {
         string multi_link = @"https://osu.ppy.sh/community/matches/";
         string base_uri = multi_link + id.ToString();
 
-        using (var client = new HttpClient())
-        {
+        using (var client = new HttpClient()) {
             var request = new HttpRequestMessage(HttpMethod.Get, base_uri);
             request.Headers.Add("Accept", @"application/json, text/javascript, */*; q=0.01");
             var response = client.Send(request);
 
-            if (response.StatusCode != HttpStatusCode.OK)
-            {
+            if (response.StatusCode != HttpStatusCode.OK) {
                 throw new Exception("not ok");
             }
 
@@ -38,17 +38,14 @@ public class Lobby
             _ = lobby.events ?? throw new Exception("no events");
             _ = lobby.users ?? throw new Exception("no users");
 
-
             var lobbyFirstEventIdSaved = lobby.events[0].id ?? throw new NullReferenceException();
             var lobbyFirstEventId = lobby.events[0].id ?? throw new NullReferenceException();
-            while (lobby.events[0].id != lobby.first_event_id)
-            {
+            while (lobby.events[0].id != lobby.first_event_id) {
                 var newRequest = new HttpRequestMessage(HttpMethod.Get, base_uri + GenerateAdditionalQueryString(lobbyFirstEventId).ToString());
                 newRequest.Headers.Add("Accept", @"application/json, text/javascript, */*; q=0.01");
 
                 var newReponse = client.Send(newRequest);
-                if (newReponse.StatusCode != HttpStatusCode.OK)
-                {
+                if (newReponse.StatusCode != HttpStatusCode.OK) {
                     throw new Exception("not ok");
                 }
 
@@ -58,56 +55,79 @@ public class Lobby
                 _ = newLobby.users ?? throw new Exception("no users");
                 lobby.events.InsertRange(0, newLobby.events);
 
-                foreach (var user in newLobby.users)
-                {
-                    if (lobby.users.Find(users => users.id == user.id) == null)
-                    {
+                foreach (var user in newLobby.users) {
+                    if (lobby.users.Find(users => users.id == user.id) == null) {
                         lobby.users.Add(user);
                     }
                 }
 
                 lobbyFirstEventId = newLobby.events[0].id ?? throw new Exception("null or something");
             }
-           
+
+            foreach (var gameEvent in lobby.events.Where(e => e.game != null)) {
+                if (gameEvent?.game?.scores?.Count == 0)
+                    lobby.AbandonedGames.Add(gameEvent);
+                else
+                    lobby.CompletedGames.Add(gameEvent ?? Enumerable.Empty<Event>().GetEnumerator().Current);
+            }
+
             return lobby;
         }
     }
 
-    public static string GenerateAdditionalQueryString(long event_id)
-    {
+    public void CalculateStats() {
+        GetWinningTeam();
+        GetPlayerStats();
+        GetHighestAverageScore();
+        GetHighestAverageAccuracy();
+    }
+
+    public static string GenerateAdditionalQueryString(long event_id) {
         return @"?before=" + event_id.ToString() + @"&limit=100";
     }
 
-    public void GetWinningTeam()
-    {
-        foreach (var gameEvent in this.events?.Where(e => e.game != null))
-        {
+    public void GetWinningTeam() {
+        foreach (var gameEvent in this.events?.Where(e => e.game != null) ?? Enumerable.Empty<Event>()) {
             if (gameEvent.game?.scores?.Count == 0) continue;
             //sum up the scores of each map and increment winner.
             long redTotalScore = 0;
             long blueTotalScore = 0;
 
-            foreach (var score in gameEvent.game?.scores?.Where(s => s.match?.team == "red"))
-            {
+            foreach (var score in gameEvent.game?.scores?.Where(s => s.match?.team == "red") ?? Enumerable.Empty<Score>()) {
                 redTotalScore += score.score;
             }
-            foreach (var score in gameEvent.game.scores.Where(s => s.match?.team == "blue"))
-            {
+            foreach (var score in gameEvent.game?.scores?.Where(s => s.match?.team == "blue") ?? Enumerable.Empty<Score>()) {
                 blueTotalScore += score.score;
             }
 
             _ = (redTotalScore > blueTotalScore) ? ++this.RedWins : ++this.BlueWins;
-
-            Console.WriteLine("id: " + gameEvent.id);
-            Console.WriteLine("map: " + gameEvent?.game?.beatmap?.beatmapset?.title);
         }
 
         _ = this.RedWins > this.BlueWins ? this.WinningTeam = "red" : this.WinningTeam = "blue";
     }
+
+    public void GetPlayerStats() {
+        foreach (var user in users ?? Enumerable.Empty<User>()) {
+            user.CalculateAverageScore(this.CompletedGames);
+            user.CalculateAverageAccuracy(this.CompletedGames);
+        }
+    }
+
+    public void GetHighestAverageScore() {
+        var baUser = this.users?.MaxBy(u => u.AverageScore);
+        var baId = baUser?.id ?? 0;
+        var baScore = baUser?.AverageScore ?? 0;
+        HighestAverageScore = (baId, baScore);
+    }
+    public void GetHighestAverageAccuracy() {
+        var baUser = this.users?.MaxBy(u => u.AverageAccuracy);
+        var baId = baUser?.id ?? 0;
+        var baAccuracy= baUser?.AverageAccuracy ?? 0;
+        HighestAverageAccuracy= (baId, baAccuracy);
+    }
 }
 
-public class Beatmap
-{
+public class Beatmap {
     public long beatmapset_id { get; set; }
     public float difficulty_rating { get; set; }
     public long id { get; set; }
@@ -119,8 +139,7 @@ public class Beatmap
     public Beatmapset? beatmapset { get; set; }
 }
 
-public class Beatmapset
-{
+public class Beatmapset {
     public string? artist { get; set; }
     public string? artist_unicode { get; set; }
     public Covers? covers { get; set; }
@@ -142,14 +161,12 @@ public class Beatmapset
     public bool video { get; set; }
 }
 
-public class Country
-{
+public class Country {
     public string? code { get; set; }
     public string? name { get; set; }
 }
 
-public class Covers
-{
+public class Covers {
     public string? cover { get; set; }
 
     [JsonPropertyName("cover@2x")]
@@ -168,19 +185,16 @@ public class Covers
     public string? slimcover2x { get; set; }
 }
 
-public class CurrentUserAttributes
-{
+public class CurrentUserAttributes {
     public object? pin { get; set; }
 }
 
-public class Detail
-{
+public class Detail {
     public string? type { get; set; }
     public string? text { get; set; }
 }
 
-public class Event
-{
+public class Event {
     public long? id { get; set; }
     public Detail? detail { get; set; }
     public DateTime timestamp { get; set; }
@@ -188,8 +202,7 @@ public class Event
     public Game? game { get; set; }
 }
 
-public class Game
-{
+public class Game {
     public long beatmap_id { get; set; }
     public long id { get; set; }
     public DateTime? start_time { get; set; }
@@ -203,8 +216,7 @@ public class Game
     public List<Score>? scores { get; set; }
 }
 
-public class Match
-{
+public class Match {
     public long id { get; set; }
     public DateTime start_time { get; set; }
     public DateTime end_time { get; set; }
@@ -214,8 +226,7 @@ public class Match
     public bool pass { get; set; }
 }
 
-public class Score
-{
+public class Score {
     public float accuracy { get; set; }
     public object? best_id { get; set; }
     public DateTime created_at { get; set; }
@@ -237,8 +248,7 @@ public class Score
     public Match? match { get; set; }
 }
 
-public class Statistics
-{
+public class Statistics {
     public int count_100 { get; set; }
     public int count_300 { get; set; }
     public int count_50 { get; set; }
@@ -247,8 +257,7 @@ public class Statistics
     public int count_miss { get; set; }
 }
 
-public class User
-{
+public class User {
     public string? avatar_url { get; set; }
     public string? country_code { get; set; }
     public string? default_group { get; set; }
@@ -264,66 +273,70 @@ public class User
     public string? username { get; set; }
     public Country? country { get; set; }
 
+    // TODO:
+    // come up with a representation of cost / impact.
     public int Impact { get; set; } = 0;
 
     public int AverageScore { get; set; } = 0;
     public int HighestScore { get; set; } = 0;
     public int LowestScore { get; set; } = 0;
+    public int MapsPlayed { get; set; } = 0;
 
     public float AverageAccuracy { get; set; } = 0;
     public float HighestAccuracy { get; set; } = 0;
     public float LowestAccuracy { get; set; } = 0;
 
-    public int? CaculateAverageScore(List<Score> scores)
-    {
+    public void CalculateAverageScore(List<Event> gameEvents) {
         int averageScore = 0;
         int scoreCount = 0;
 
-        foreach (var score in scores.Where(s => s.user_id == this.id))
-        {
-            averageScore += score.score;
+        foreach (var scores in gameEvents.
+            Where(ge => ge.game?.scores?
+                .Exists(s => s.user_id == this.id) ?? false)
+            .Select(ge => ge.game?.scores)) {
+
+            averageScore += scores?.First(s => s.user_id == this.id).score ?? 0;
             ++scoreCount;
         }
 
-        if (scoreCount == 0)
-        {
-            return null;
-        }
+        if (scoreCount == 0) return;
 
-        return averageScore / scoreCount;
+        AverageScore = averageScore / scoreCount;
+        MapsPlayed = scoreCount;
     }
 
-    public int GetHighestScore(List<Score> scores)
-    {
+    public int GetHighestScore(List<Score> scores) {
         return scores.Where(s => s.user_id == this.id).Max(s => s.score);
     }
 
-    public int GetLowestScore(List<Score> scores)
-    {
+    public int GetLowestScore(List<Score> scores) {
         return scores.Where(s => s.user_id == this.id).Min(s => s.score);
     }
 
-    public float CaculateAverageAccuracy(List<Score> scores)
-    {
+    public void CalculateAverageAccuracy(List<Event> gameEvents) {
         float averageAccuracy = 0;
         int scoreCount = 0;
 
-        foreach (var score in scores.Where(s => s.user_id == this.id))
-        {
-            averageAccuracy += score.accuracy;
+        foreach (var scores in gameEvents
+            .Where(ge => ge.game?.scores?
+                .Exists(s => s.user_id == this.id) ?? false)
+            .Select(ge => ge.game?.scores)) {
+
+            averageAccuracy += scores?.First(s => s.user_id == this.id).accuracy ?? 0;
             ++scoreCount;
         }
 
-        return averageAccuracy / scoreCount;
+        if (scoreCount == 0) return;
+
+        AverageAccuracy = (float)Math.Round((averageAccuracy / scoreCount) * 100, 2);
+        MapsPlayed = scoreCount;
     }
 
-    public float GetHighestAccuracy(List<Score> scores)
-    {
+    public float GetHighestAccuracy(List<Score> scores) {
         return scores.Where(s => s.user_id == this.id).Max(s => s.accuracy);
     }
 
-    public float GetLowestAccuracy(List<Score> scores)
-    {
+    public float GetLowestAccuracy(List<Score> scores) {
         return scores.Where(s => s.user_id == this.id).Min(s => s.accuracy);
     }
 }
